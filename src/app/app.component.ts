cb1125c077570117ec935c990773de87ce54ec00
retrofuturo <pzabelina11@gmail.com>
2020-03-30 09:58:11 +0300

    test-timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, Subscription} from 'rxjs/index';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'timer-task';
+
+  interval: Subscription;
+  timeLabel = '00:00:00';
+  timer = 0;
+  waitObservable: Subscription;
+
+  startTimer() {
+    if (!this.interval) {
+      this.interval = interval(1000)
+        .subscribe(() => {
+          this.timer += 1;
+          this.updateTime();
+        });
+    }
+  }
+
+  stopTimer() {
+    if (this.interval) {
+      this.interval.unsubscribe();
+      this.interval = null;
+    }
+  }
+
+  resetTimer() {
+    this.stopTimer();
+    this.timer = 0;
+    this.updateTime();
+  }
+
+  wait() {
+    if (this.waitObservable) {
+      this.stopTimer();
+    }
+    if (!this.waitObservable) {
+      this.waitObservable = interval(300)
+        .subscribe(() => {
+          this.waitObservable.unsubscribe();
+          this.waitObservable = null;
+        });
+    }
+
+  }
+
+  updateTime() {
+    const hours = Math.floor((this.timer / 3600) % 24 );
+    const minutes = Math.floor((this.timer / 60) % 60 );
+    const seconds = Math.floor(this.timer % 60);
+    this.timeLabel = `${this.timeToString(hours)}:${this.timeToString(minutes)}:${this.timeToString(seconds)}`;
+  }
+
+  private timeToString(count) {
+    return count < 10 ? `0${Math.floor(count)}` : Math.floor(count).toString();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
